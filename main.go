f593ec9842f537cbdd3fa868871d1d4bba5b4255
Matthew Dobson <mdobson4@gmail.com>
2017-01-07 14:46:08 -0500

    Adding mux

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"handlers"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/rand", handlers.HandleGenerateSig)
+	r.HandleFunc("/foo", handlers.HandleFooRequest)
+	r.HandleFunc("/", handlers.HandleBasicRequest)
 
-	http.HandleFunc("/rand", handlers.HandleGenerateSig)
-	http.HandleFunc("/foo", handlers.HandleFooRequest)
-	http.HandleFunc("/", handlers.HandleBasicRequest)
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
