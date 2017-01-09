12ff3919726121bf400bd61b6ee34c5246a823ff
Matthew Dobson <mdobson4@gmail.com>
2017-01-08 19:26:55 -0500

    Reading yaml. Doing stuff with the yaml.

--- main.go
+++ main.go
@@ -6,14 +6,52 @@ import (
 	"net/http"
 	"proxy"
 
+	yaml "gopkg.in/yaml.v2"
+
+	"io/ioutil"
+
+	"fmt"
+
 	"github.com/gorilla/mux"
 )
 
 func main() {
 
+	data, err := ioutil.ReadFile("./proxy.yaml")
+	fmt.Println(string(data))
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+	}
+
+	obj := make(map[interface{}]interface{})
+	err = yaml.Unmarshal([]byte(data), &obj)
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+	}
+
+	fmt.Printf("--- m:\n%v\n\n", obj["proxies"])
+
 	p := proxy.New("http://mocktarget.apigee.net")
 
 	r := mux.NewRouter()
+
+	for _, proxyData := range obj["proxies"].(map[interface{}]interface{}) {
+
+		m2 := make(map[string]string)
+		for key, value := range proxyData.(map[interface{}]interface{}) {
+			switch key := key.(type) {
+			case string:
+				switch value := value.(type) {
+				case string:
+					m2[key] = value
+				}
+			}
+		}
+
+		p = proxy.New(m2["url"])
+		r.HandleFunc(m2["base_path"], p.Handle)
+	}
+
 	r.HandleFunc("/rand", handlers.HandleGenerateSig)
 	r.HandleFunc("/foo", handlers.HandleFooRequest)
 	r.HandleFunc("/keys", handlers.HandleGenerateKeyPair)
